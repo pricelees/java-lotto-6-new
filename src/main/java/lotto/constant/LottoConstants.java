afa39f82c87a61d4bcd16b3554b167176f59aed1
sangdol <priceelees@gmail.com>
2023-12-01 00:11:39 +0900

    refactor(constant): 상수 인터페이스를 final class로 수정

--- src/main/java/lotto/constant/LottoConstants.java
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -1,10 +1,13 @@
 package lotto.constant;
 
-public interface LottoConstants {
-    String LINE_SEPARATOR = System.lineSeparator();
-    int ZERO = 0;
-    int ONE_LOTTO_PRICE = 1000;
-    int LOTTO_NUMBER_COUNT = 6;
-    int LOTTO_MIN_NUMBER = 1;
-    int LOTTO_MAX_NUMBER = 45;
+public final class LottoConstants {
+    private LottoConstants() {
+    }
+
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+    public static final int ZERO = 0;
+    public static final int ONE_LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
 }
