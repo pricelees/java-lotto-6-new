ba223bcfceb25f20f586929d2204072ba6d97fc9
sangdol <priceelees@gmail.com>
2023-11-30 22:05:20 +0900

    feat(domain): 로또 당첨 정보를 저장하고, 당첨금을 계산하는 LottoWinningResult 클래스 정의

---
+++ src/main/java/lotto/domain/LottoWinningResult.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
+public class LottoWinningResult {
+    private final EnumMap<LottoWinningType, Integer> allTypesCount;
+
+    public LottoWinningResult(EnumMap<LottoWinningType, Integer> allTypesCount) {
+        this.allTypesCount = allTypesCount;
+    }
+
+    public long calculateTotalWinningAmount() {
+        return allTypesCount.keySet().stream()
+                .mapToInt(type -> type.getWinningAmount() * allTypesCount.get(type))
+                .sum();
+    }
+
+    public List<Integer> getMatchCount() {
+        return new ArrayList<>(allTypesCount.values());
+    }
+}
