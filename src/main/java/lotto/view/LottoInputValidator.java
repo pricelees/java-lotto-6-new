837a724e5e9c471b84bccd5a73394c1be1863ee8
sangdol <priceelees@gmail.com>
2023-11-30 22:11:46 +0900

    feat(view): 로또 게임과 관련된 입력에 대한 유효성을 검증하는 LottoInputValidator 클래스 정의

---
+++ src/main/java/lotto/view/LottoInputValidator.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+public class LottoInputValidator {
+    private static final String NOT_NUMBER = "숫자만 입력 가능합니다.";
+    private static final String INVALID_WINNING_NUMBER_FORMAT = "입력 형식이 올바르지 않습니다.";
+    private static final String WINNING_NUMBER_FORMAT_REGEX = "([0-9]+)(,([0-9]+)){5}";
+
+    public static void validateContainsOnlyNumber(String input) {
+        try {
+            Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER);
+        }
+    }
+
+    public static void validateWinningNumber(String input) {
+        if (isInvalidWinningNumberFormat(input)) {
+            throw new IllegalArgumentException(INVALID_WINNING_NUMBER_FORMAT);
+        }
+    }
+
+
+    private static boolean isInvalidWinningNumberFormat(String input) {
+        return !input.matches(WINNING_NUMBER_FORMAT_REGEX);
+    }
+}
