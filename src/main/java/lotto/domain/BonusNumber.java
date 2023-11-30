3bc6be052345773eac46d304558a1b3dd348e032
sangdol <priceelees@gmail.com>
2023-11-30 22:01:04 +0900

    feat(domain): 보너스 번호를 저장하며 숫자 범위에 대한 예외를 검증하는 BonusNumber 클래스 정의

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import lotto.constant.LottoConstants;
+
+public class BonusNumber {
+    private static final String INVALID_RANGE = "숫자 범위가 유효하지 않습니다.";
+    private final int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        validate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private static void validate(int bonusNumber) {
+        if (isInvalidNumber(bonusNumber)) {
+            throw new IllegalArgumentException(INVALID_RANGE);
+        }
+    }
+
+    private static boolean isInvalidNumber(int bonusNumber) {
+        return bonusNumber > LottoConstants.LOTTO_MAX_NUMBER || bonusNumber < LottoConstants.LOTTO_MIN_NUMBER;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
