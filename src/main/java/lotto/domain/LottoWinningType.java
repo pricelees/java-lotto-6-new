0e9ca805e5ac4ecd8a80cd1a9a4896f2d571d51c
sangdol <priceelees@gmail.com>
2023-11-30 22:02:22 +0900

    feat(domain): 로또의 당첨 경우와, 당첨금, 당첨 조건을 저장하는 LottoWinningType Enum 정의
    < description >
    - 당첨 갯수와 보너스 번호 포함 여부를 받아 상수를 얻어낼 수 있음
    - <당첨 종류, 당첨 횟수>를 저장하는 Map을 초기화하는 기능 정의

---
+++ src/main/java/lotto/domain/LottoWinningType.java
@@ -0,0 +1,64 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.function.BiPredicate;
+import lotto.constant.LottoConstants;
+
+public enum LottoWinningType {
+    THREE_NUMBER_MATCHED(
+            5_000,
+            (matchCount, hasBonusNumber) -> matchCount == 3
+    ),
+    FOUR_NUMBER_MATCHED(
+            50_000,
+            (matchCount, hasBonusNumber) -> matchCount == 4
+    ),
+    FIVE_NUMBER_MATCHED(
+            1_500_000,
+            (matchCount, hasBonusNumber) -> matchCount == 5 && !hasBonusNumber
+    ),
+    FIVE_AND_BONUS_MATCHED(
+            30_000_000,
+            (matchCount, hasBonusNumber) -> matchCount == 5 && hasBonusNumber
+    ),
+    SIX_NUMBER_MATCHED(
+            2_000_000_000,
+            (matchCount, hasBonusNumber) -> matchCount == 6
+    ),
+    NONE(
+            0,
+            (matchCount, hasBonusNumber) -> matchCount < 3
+    );
+
+    private final int winningAmount;
+    private final BiPredicate<Integer, Boolean> condition;
+
+    LottoWinningType(int winningAmount, BiPredicate<Integer, Boolean> condition) {
+        this.winningAmount = winningAmount;
+        this.condition = condition;
+    }
+
+    public static boolean isWin(LottoWinningType lottoWinningType) {
+        return lottoWinningType != NONE;
+    }
+
+    public static LottoWinningType from(int matchCount, boolean hasBonusNumber) {
+        return Arrays.stream(LottoWinningType.values())
+                .filter(result -> result.condition.test(matchCount, hasBonusNumber))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static EnumMap<LottoWinningType, Integer> createInitCountsMap() {
+        EnumMap<LottoWinningType, Integer> result = new EnumMap<>(LottoWinningType.class);
+        Arrays.stream(LottoWinningType.values())
+                .filter(LottoWinningType::isWin)
+                .forEach(type -> result.put(type, LottoConstants.ZERO));
+        return result;
+    }
+
+    public int getWinningAmount() {
+        return winningAmount;
+    }
+}
