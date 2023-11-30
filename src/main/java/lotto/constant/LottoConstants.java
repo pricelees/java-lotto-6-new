8636dba03d8ca5734e24b0d3d69ad5ac201c3292
sangdol <priceelees@gmail.com>
2023-11-30 21:59:46 +0900

    feat(constants): LottoConstants 클래스에 프로그램에서 전반적으로 쓰이는 상수를 별도로 정의

---
+++ src/main/java/lotto/constant/LottoConstants.java
@@ -0,0 +1,10 @@
+package lotto.constant;
+
+public interface LottoConstants {
+    String LINE_SEPARATOR = System.lineSeparator();
+    int ZERO = 0;
+    int ONE_LOTTO_PRICE = 1000;
+    int LOTTO_NUMBER_COUNT = 6;
+    int LOTTO_MIN_NUMBER = 1;
+    int LOTTO_MAX_NUMBER = 45;
+}
