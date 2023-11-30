933687cfdaaffbc6feec8d4699f047d71fc6b809
sangdol <priceelees@gmail.com>
2023-11-30 22:09:32 +0900

    feat(service): 로또 당첨 확인과 관련된 비즈니스 로직이 구현된 LottoCompareService 클래스 정의

---
+++ src/main/java/lotto/service/LottoCompareService.java
@@ -0,0 +1,54 @@
+package lotto.service;
+
+import java.util.EnumMap;
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.LottoWinningResult;
+import lotto.domain.LottoWinningType;
+import lotto.domain.UserLotto;
+import lotto.domain.WinningLotto;
+import lotto.dto.request.BonusNumberRequest;
+import lotto.dto.request.WinningNumberRequest;
+import lotto.dto.response.WinningResponse;
+
+public class LottoCompareService {
+    private static final int COUNT_UNIT = 1;
+    private static final int HUNDRED = 100;
+    public WinningLotto receiveWinningLottoInfo(WinningNumberRequest winningNumberRequest, BonusNumberRequest bonusNumberRequest) {
+        return WinningLotto.valueOf(
+                winningNumberRequest.winningNumber(),
+                bonusNumberRequest.bonusNumber()
+        );
+    }
+
+    public LottoWinningResult compare(UserLotto userLotto, WinningLotto winningLotto) {
+        return new LottoWinningResult(compare(
+                userLotto.getLottos(), winningLotto.getNumbers(), winningLotto.getBonusNumber()
+        ));
+    }
+
+    private EnumMap<LottoWinningType, Integer> compare(List<Lotto> lottos, Lotto winningLotto,
+                                                       BonusNumber bonusNumber) {
+        EnumMap<LottoWinningType, Integer> eachResultsCount = LottoWinningType.createInitCountsMap();
+        lottos.stream()
+                .map(lotto -> {
+                    int matchCount = lotto.calculateMatchedCount(winningLotto);
+                    boolean hasBonusNumber = lotto.isContain(bonusNumber.getBonusNumber());
+                    return LottoWinningType.from(matchCount, hasBonusNumber);
+                })
+                .filter(LottoWinningType::isWin)
+                .forEach(lottoWinningType -> eachResultsCount.merge(lottoWinningType, COUNT_UNIT, Integer::sum));
+
+        return eachResultsCount;
+    }
+
+    public WinningResponse receiveResults(LottoWinningResult lottoWinningResult, long purchaseAmount) {
+        double earningRate = calculateEarningRate(purchaseAmount, lottoWinningResult.calculateTotalWinningAmount());
+        return new WinningResponse(lottoWinningResult.getMatchCount(), earningRate);
+    }
+
+    private double calculateEarningRate(long purchaseAmount, long winningAmount) {
+        return ((double)winningAmount / purchaseAmount) * HUNDRED;
+    }
+}
