5d1f90e6e0f7e7aecea0f4084523c6184ef62cfa
sangdol <priceelees@gmail.com>
2023-12-01 12:13:19 +0900

    style(request): LottoPurchaseRequest 클래스에서의 예외 메시지 수정

--- src/main/java/lotto/dto/request/LottoPurchaseRequest.java
+++ src/main/java/lotto/dto/request/LottoPurchaseRequest.java
@@ -3,7 +3,7 @@ package lotto.dto.request;
 import lotto.constant.LottoConstants;
 
 public record LottoPurchaseRequest(long amount) {
-    private static final String NOT_ENOUGH_AMOUNT = "1000원 이상의 금액을 입력해주세요.";
+    private static final String NOT_ENOUGH_AMOUNT = "1000원 미만의 금액은 입력할 수 없습니다.";
     private static final String NOT_DIVIDED_BY_THOUSAND = "금액은 1000원 단위어야 합니다.";
 
     public LottoPurchaseRequest {
