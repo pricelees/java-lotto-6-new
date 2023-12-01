a17ab2f282b85e5006b020c61f6ee77f85cc4e7f
sangdol <priceelees@gmail.com>
2023-12-01 14:44:08 +0900

    refactor(view): LottoInputValidator에서 Long.parseLong이 아닌 정규식을 이용하여 숫자 범위를 검증하도록 수정

--- src/main/java/lotto/view/LottoInputValidator.java
+++ src/main/java/lotto/view/LottoInputValidator.java
@@ -3,17 +3,20 @@ package lotto.view;
 public class LottoInputValidator {
     private static final String NOT_NUMBER = "숫자만 입력 가능합니다.";
     private static final String INVALID_WINNING_NUMBER_FORMAT = "입력 형식이 올바르지 않습니다.";
+    private static final String ONLY_NUMBER_FORMAT_REGEX = "(-)?(\\d)+";
     private static final String WINNING_NUMBER_FORMAT_REGEX = "(\\d+)(,(\\d+)){5}";
 
     private LottoInputValidator() {
     }
 
     public static void validateContainsOnlyNumber(String input) {
-        try {
-            Long.parseLong(input);
-        } catch (NumberFormatException e) {
+        if (hasNonDigits(input)) {
             throw new IllegalArgumentException(NOT_NUMBER);
         }
+    }
+
+    private static boolean hasNonDigits(String input) {
+        return !input.matches(ONLY_NUMBER_FORMAT_REGEX);
     }
 
     public static void validateWinningNumber(String input) {
@@ -21,7 +24,6 @@ public class LottoInputValidator {
             throw new IllegalArgumentException(INVALID_WINNING_NUMBER_FORMAT);
         }
     }
-
 
     private static boolean isInvalidWinningNumberFormat(String input) {
         return !input.matches(WINNING_NUMBER_FORMAT_REGEX);
